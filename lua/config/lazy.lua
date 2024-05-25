c48105e471f0e263dee4fc2f369b71bfeb39abf6
bruno-br <bruno.brasilf@gmail.com>
2024-05-25 20:51:13 -0300

    add config outline plugin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,6 +13,7 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.coding.yanky" },
+    { import = "lazyvim.plugins.extras.editor.outline" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.elixir" },
     { import = "lazyvim.plugins.extras.lang.go" },
