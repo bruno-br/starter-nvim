c00e05f42e1a97676f95373f2145d134298a2b7d
bruno-br <bruno.brasilf@gmail.com>
2024-05-25 20:51:48 -0300

    add nvim tmux navigator

---
+++ lua/plugins/nvim-tmux-navigator.lua
@@ -0,0 +1,9 @@
+return {
+    "christoomey/vim-tmux-navigator",
+    keys = {
+        { "<C-h>", "<cmd>TmuxNavigateLeft<cr>",  desc = "Pane left" },
+        { "<C-j>", "<cmd>TmuxNavigateDown<cr>",  desc = "Pane down" },
+        { "<C-k>", "<cmd>TmuxNavigateUp<cr>",    desc = "Pane up" },
+        { "<C-l>", "<cmd>TmuxNavigateRight<cr>", desc = "Pane right" },
+    },
+}
